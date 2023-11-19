53952cb88c974cf35528831e04bbefd58d6f2b00
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2023-11-19 10:17:54 +0330

    add port config

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config['MYSQL_HOST']     = os.getenv('DB_HOST', 'localhost')
 app.config['MYSQL_USER']     = os.getenv('DB_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD', '')
 app.config['MYSQL_DB']       = os.getenv('DB_NAME', 'db')
+app.config['MYSQL_PORT']     = int(os.getenv('DB_PORT', 3306)) 
 mysql = MySQL(app)
 
 # Configuring email settings
