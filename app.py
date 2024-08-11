6ce38e2ba1f538e84cd832a8cd690517913c2205
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-08-11 12:54:47 +0330

    bump to flask 3.0.3

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
