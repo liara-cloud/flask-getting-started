9b857c100a07f7e65711ee6fe355b36be7d34310
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-12-06 14:16:24 +0330

    add cc bcc

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask, request
 from flask_mail import Mail, Message
-from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv()
+# # uncomment in development environment
+# from dotenv import load_dotenv
+# load_dotenv()
 
 app = Flask(__name__)
 
-# Flask-Mail configuration
 app.config['MAIL_SERVER'] = os.getenv('MAIL_HOST')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USER')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = True  # Use TLS for encryption
-app.config['MAIL_USE_SSL'] = False  # Don't use SSL, because we are using TLS
+app.config['MAIL_USE_TLS'] = True 
+app.config['MAIL_USE_SSL'] = False 
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_FROM_ADDRESS')
 
-# Initialize Flask-Mail
+cc_mail = ['alinajmabadizadeh2002@gmail.com', 'alinajmabadi@liara.ir']
+# bcc_mail = ['alinajmabadizadeh2002@gmail.com', 'alinajmabadi@liara.ir']
+
 mail = Mail(app)
 
 @app.route('/send-test-email', methods=['GET'])
 def send_test_email():
     try:
-        # Create a message
         msg = Message(subject='Test Email from Flask',
-                      recipients=['recipient@example.com'],  # Replace with the recipient's email address
+                      recipients=['some@looms.ir'],  
                       body='This is a test email sent from Flask using SMTP on Liara.',
+                      cc=cc_mail,
+                    #   bcc=bcc_mail,
                       extra_headers = {"x-liara-tag": "test_tag"})
 
-        # Send the email
         mail.send(msg)
         return 'Test email sent successfully!', 200
 
